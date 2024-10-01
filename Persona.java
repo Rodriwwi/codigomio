5300c481f084c56e45e6ab6ce4880fab6013ed99
Rodriwwi <148747969+Rodriwwi@users.noreply.github.com>
2024-10-01 10:32:20 +0200

    Create Persona.java

---
+++ Persona.java
@@ -0,0 +1,74 @@
+package es.unex.cum.mdp.sesion03;
+
+public class Persona {
+    private String nombre;
+    private String id;
+    private int edad;
+
+    
+
+    public Persona(){
+        this.nombre = "nada";
+        this.id = "nada";
+        this.edad = 0;
+    }
+    public Persona(String nombre, String id, int edad) {
+        this.nombre = nombre;
+        this.id = id;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Persona other = (Persona) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona [nombre=" + nombre + ", id=" + id + ", edad=" + edad + "]";
+    }
+}
